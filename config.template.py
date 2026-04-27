08d9992435687c3e5e4aaf163442c1e9ed551b06
Shio Chiba <shionatania@gmail.com>
2026-04-27 14:07:06 -0700

    update config template with all data paths

--- config.template.py
+++ config.template.py
@@ -1,4 +1,6 @@
 # config.template.py — copy this to config.py and fill in your paths
 # Do not put real paths in this file
 
-DATA_DIR = ""
+PROJECT_DIR = ""
+VOXELWISE_DATA_DIR = ""
+BOLD_DIR = ""  # only available on lab machine, leave empty on local
